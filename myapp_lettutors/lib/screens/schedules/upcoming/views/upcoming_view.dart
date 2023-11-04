c05d6a569b63653a98ce2a837ca40dd4028b66b6
TranTrongDai <trongdaitran0903@gmail.com>
2023-11-04 23:11:05 +0700

    update Upcomming tutors + remove some code trash

--- myapp_lettutors/lib/screens/schedules/upcoming/views/upcoming_view.dart
+++ myapp_lettutors/lib/screens/schedules/upcoming/views/upcoming_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:myapp_lettutors/constants/items_per_page.dart';
 import 'package:myapp_lettutors/models/schedule/booking_info.dart';
 import 'package:myapp_lettutors/providers/auth_provider.dart';
@@ -17,9 +18,8 @@ class UpcomingView extends StatefulWidget {
 class _UpcomingClassViewState extends State<UpcomingView> {
   List<BookingInfo> upcoming = [];
 
-  int _page = 1;
-  int _perPage = itemsPerPage.first;
-  int _count = 0;
+  final int _page = 1;
+  final int _perPage = itemsPerPage.first;
   bool _isLoading = true;
 
   Future<void> _fetchUpcomingClasses(String token) async {
@@ -31,7 +31,6 @@ class _UpcomingClassViewState extends State<UpcomingView> {
 
     setState(() {
       upcoming = result['classes'];
-      _count = result['count'];
       _isLoading = false;
     });
   }
@@ -49,147 +48,49 @@ class _UpcomingClassViewState extends State<UpcomingView> {
         ? const Center(
             child: CircularProgressIndicator(),
           )
-        : upcoming.isEmpty
-            ? const Center(
-                child: Text(
-                  'You have no upcoming class',
-                  style: TextStyle(fontSize: 16),
+        : SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text(
+                  'You have 10 upcoming classes',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
-              )
-            : SingleChildScrollView(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Text(
-                      'You have $_count upcoming classes',
-                      style: Theme.of(context).textTheme.headlineMedium,
+                const SizedBox(height: 16),
+                const SizedBox(height: 8),
+                ...List<Widget>.generate(
+                  5,
+                  (index) => UpcomingClassCard(
+                    bookingInfo: BookingInfo(
+                      createdAtTimeStamp: DateTime.now().millisecondsSinceEpoch,
+                      updatedAtTimeStamp: DateTime.now().millisecondsSinceEpoch,
+                      id: "null id",
+                      userId: "userId",
+                      scheduleDetailId: "scheduleDetailId",
+                      tutorMeetingLink: "",
+                      studentMeetingLink: "",
+                      studentRequest: "Request from student",
+                      tutorReview: "Tutor review",
+                      scoreByTutor: 3,
+                      createdAt: DateFormat().add_yMEd().format(DateTime.now()),
+                      updatedAt: DateFormat().add_yMEd().format(DateTime.now()),
+                      recordUrl: "",
+                      isDeleted: false,
+                      scheduleDetailInfo: null,
+                      classReview: null,
                     ),
-                    const SizedBox(height: 16),
-                    Row(
-                      children: [
-                        const Expanded(
-                          flex: 20,
-                          child: Text(
-                            'Items per page',
-                            textAlign: TextAlign.right,
-                          ),
-                        ),
-                        const SizedBox.shrink(),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          flex: 7,
-                          child: DropdownButtonFormField<int>(
-                            value: _perPage,
-                            items: itemsPerPage
-                                .map((itemPerPage) => DropdownMenuItem<int>(
-                                    value: itemPerPage,
-                                    child: Text('$itemPerPage')))
-                                .toList(),
-                            onChanged: (value) {
-                              setState(() {
-                                _perPage = value!;
-                                _page = 1;
-                                _isLoading = true;
-                              });
-                            },
-                            icon: const Icon(
-                              Icons.keyboard_arrow_down_rounded,
-                              color: Colors.blue,
-                            ),
-                            decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.symmetric(
-                                  vertical: 4, horizontal: 16),
-                              filled: true,
-                              fillColor: Colors.blue[50],
-                              enabledBorder: const OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.transparent),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(24)),
-                              ),
-                              focusedBorder: const OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.transparent),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(24)),
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    ...List<Widget>.generate(
-                      upcoming.length,
-                      (index) => UpcomingClassCard(
-                        bookingInfo: upcoming[index],
-                        onCancel: (value) {
-                          if (value) {
-                            setState(() {
-                              _isLoading = true;
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      children: [
-                        IconButton(
-                          style: IconButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            backgroundColor:
-                                _page == 1 ? Colors.grey : Colors.blue[300],
-                          ),
-                          onPressed: _page == 1
-                              ? null
-                              : () {
-                                  setState(() {
-                                    _isLoading = true;
-                                    _page--;
-                                  });
-                                },
-                          icon: const Icon(
-                            Icons.navigate_before_rounded,
-                            size: 28,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            'Page $_page/${(_count / _perPage).ceil()}',
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        IconButton(
-                          style: IconButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            backgroundColor: _page == _count
-                                ? Colors.grey
-                                : Colors.blue[300],
-                          ),
-                          onPressed: _page == (_count / _perPage).ceil()
-                              ? null
-                              : () {
-                                  setState(() {
-                                    _isLoading = true;
-                                    _page++;
-                                  });
-                                },
-                          icon: const Icon(
-                            Icons.navigate_next_rounded,
-                            size: 28,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                    onCancel: (value) {
+                      if (value) {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                      }
+                    },
+                  ),
                 ),
-              );
+                const SizedBox(height: 8),
+              ],
+            ),
+          );
   }
 }
--- myapp_lettutors/lib/screens/schedules/upcoming/widgets/upcoming_card.dart
+++ myapp_lettutors/lib/screens/schedules/upcoming/widgets/upcoming_card.dart
@@ -1,18 +1,8 @@
-import 'dart:io';
-
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-// import 'package:jitsi_meet/feature_flag/feature_flag.dart';
-// import 'package:jitsi_meet/jitsi_meet.dart';
-import 'package:jwt_decoder/jwt_decoder.dart';
-import 'package:myapp_lettutors/constants/routes.dart';
-// import 'package:myapp_lettutors/features/video_call/video_call_view.dart';
+import 'package:jitsi_meet_wrapper/jitsi_meet_wrapper.dart';
 import 'package:myapp_lettutors/models/schedule/booking_info.dart';
-import 'package:myapp_lettutors/providers/auth_provider.dart';
-import 'package:myapp_lettutors/services/booking_service.dart';
-import 'package:provider/provider.dart';
 
 class UpcomingClassCard extends StatelessWidget {
   const UpcomingClassCard(
@@ -22,81 +12,41 @@ class UpcomingClassCard extends StatelessWidget {
   final BookingInfo bookingInfo;
   final Function(bool cancelResult) onCancel;
 
-  String _convertClassTime() {
-    String result = '';
-    result += DateFormat.Hm().format(DateTime.fromMillisecondsSinceEpoch(
-        bookingInfo.scheduleDetailInfo!.startPeriodTimestamp ?? 0));
-    result += ' - ';
-    result += DateFormat.Hm().format(DateTime.fromMillisecondsSinceEpoch(
-        bookingInfo.scheduleDetailInfo!.endPeriodTimestamp ?? 0));
-    return result;
+  void _joinMeeting() async {
+    Map<String, Object> featureFlags = {};
+    try {
+      var options = JitsiMeetingOptions(
+        roomNameOrUrl: "learningRoom",
+        serverUrl: "https://meet.lettutor.com",
+        isAudioMuted: false,
+        isAudioOnly: false,
+        isVideoMuted: false,
+        userDisplayName: "Test name",
+        userEmail: "trongdaidu@gmail.com",
+        userAvatarUrl:
+            'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg',
+        featureFlags: featureFlags,
+      );
+
+      await JitsiMeetWrapper.joinMeeting(options: options);
+    } catch (e) {
+      print('Error: $e');
+      // }
+    }
   }
-
-  Future<String> _handleCancelClass(AuthProvider authProvider) async {
-    final String token = authProvider.token?.access?.token as String;
-    final result = await BookingService.cancelBookedClass(
-      scheduleDetailIds: [bookingInfo.id ?? ''],
-      token: token,
-    );
-    return result;
-  }
-
-  bool _isTimeToJoin() {
-    final startTimestamp =
-        bookingInfo.scheduleDetailInfo?.startPeriodTimestamp ?? 0;
-    final startTime = DateTime.fromMillisecondsSinceEpoch(startTimestamp);
-    final now = DateTime.now();
-    return now.isAfter(startTime) || now.isAtSameMomentAs(startTime);
-  }
-
-  // void _joinMeeting(String room, String meetingToken) async {
-  //   Map<FeatureFlagEnum, bool> featureFlags = {
-  //     FeatureFlagEnum.WELCOME_PAGE_ENABLED: false,
-  //   };
-  //   if (!kIsWeb) {
-  //     if (Platform.isAndroid) {
-  //       featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
-  //     } else if (Platform.isIOS) {
-  //       featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
-  //     }
-  //   }
-
-  //   final options = JitsiMeetingOptions(room: room)
-  //     // ..serverURL = 'https://meet.jit.si/'
-  //     ..serverURL = "https://meet.lettutor.com"
-  //     ..token = meetingToken
-  //     ..audioOnly = true
-  //     ..audioMuted = true
-  //     ..videoMuted = true
-  //     ..featureFlags.addAll(featureFlags);
-  //   await JitsiMeet.joinMeeting(options);
-  // }
 
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
-
-    final String meetingToken =
-        bookingInfo.studentMeetingLink?.split('token=')[1] ?? '';
-    Map<String, dynamic> jwtDecoded = JwtDecoder.decode(meetingToken);
-    final String room = jwtDecoded['room'];
-    // print(jwtDecoded['room']);
-    // const room = 'abcd';
-
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 12),
       surfaceTintColor: Colors.white,
       elevation: 2,
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 16, 8, 8),
+        padding: const EdgeInsets.all(8.0).copyWith(top: 16),
         child: Column(
           children: [
             Row(
               children: [
-                // const CircleAvatar(
-                //   backgroundImage: AssetImage('assets/tutor/keegan-avatar.png'),
-                //   radius: 32,
-                // ),
                 Container(
                   width: 72,
                   height: 72,
@@ -105,9 +55,8 @@ class UpcomingClassCard extends StatelessWidget {
                     shape: BoxShape.circle,
                   ),
                   child: CachedNetworkImage(
-                    imageUrl: bookingInfo.scheduleDetailInfo!.scheduleInfo!
-                            .tutorInfo!.avatar ??
-                        '',
+                    imageUrl:
+                        'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg',
                     fit: BoxFit.cover,
                     errorWidget: (context, url, error) => const Icon(
                       Icons.error_outline_rounded,
@@ -122,22 +71,20 @@ class UpcomingClassCard extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        bookingInfo.scheduleDetailInfo!.scheduleInfo!.tutorInfo!
-                                .name ??
-                            'null name',
+                        'null name',
                         style: Theme.of(context).textTheme.headline4,
                       ),
                       const SizedBox(height: 8),
                       Text(
                         DateFormat.yMMMEd().format(
                             DateTime.fromMillisecondsSinceEpoch(bookingInfo
-                                    .scheduleDetailInfo!.startPeriodTimestamp ??
+                                    .scheduleDetailInfo?.startPeriodTimestamp ??
                                 0)),
                         style: const TextStyle(
                             fontSize: 15, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        _convertClassTime(),
+                        DateFormat().add_yMEd().format(DateTime.now()),
                         style: const TextStyle(fontSize: 15),
                       ),
                     ],
@@ -182,28 +129,6 @@ class UpcomingClassCard extends StatelessWidget {
                           ],
                         ),
                       );
-                      if (dialogResult) {
-                        final result = await _handleCancelClass(authProvider);
-                        // ignore: use_build_context_synchronously
-                        showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                            content: Text(result),
-                            actions: [
-                              TextButton(
-                                onPressed: () {
-                                  if (result ==
-                                      "Class Cancelled Successfully") {
-                                    onCancel(true);
-                                  }
-                                  Navigator.pop(context, false);
-                                },
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          ),
-                        );
-                      }
                     },
                     child: const Text(
                       'Cancel',
@@ -214,33 +139,8 @@ class UpcomingClassCard extends StatelessWidget {
                 const SizedBox(width: 12),
                 Expanded(
                   child: TextButton(
-                    onPressed: () async {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) {
-                      //       final start = bookingInfo.scheduleDetailInfo!.startPeriodTimestamp!;
-                      //       return VideoCallView(startTimestamp: start);
-                      //     },
-                      //   ),
-                      // );
-
-                      // if (_isTimeToJoin()) {
-                      //   _joinMeeting(room, meetingToken);
-                      // } else {
-                      //   final result = await showWaitingRoomDialog(context);
-                      //   if (result) {
-                      //     _joinMeeting(room, meetingToken);
-                      //   } else {
-                      //     Navigator.push(context, MaterialPageRoute(
-                      //       builder: (context) {
-                      //         final start = bookingInfo
-                      //             .scheduleDetailInfo!.startPeriodTimestamp!;
-                      //         return VideoCallView(startTimestamp: start);
-                      //       },
-                      //     ));
-                      //   }
-                      // }
+                    onPressed: () {
+                      _joinMeeting();
                     },
                     child: const Text(
                       'Go to meeting',
